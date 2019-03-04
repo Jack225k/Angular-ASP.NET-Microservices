c8dbfbb2ad15069739799e24b830c218bb1275b2
kaybo <kotzecabo@live.com>
2019-03-04 11:16:35 +0200

    Add Chart.js componenets

--- src/app/charts/bar-chart/bar-chart.component.ts
+++ src/app/charts/bar-chart/bar-chart.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+const SAMPLE_DATA: any[] = [
+  {data:[65, 59, 80, 81, 56, 54, 30], label: 'Fall Sales'},
+  {data:[30, 37, 78, 23, 48, 20, 47], label: 'Winter Sales'}
+];
+
+const SAMPLE_DATA_LABELS: string[] = ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7'];
+
 @Component({
   selector: 'app-bar-chart',
   templateUrl: './bar-chart.component.html',
@@ -8,6 +15,15 @@ import { Component, OnInit } from '@angular/core';
 export class BarChartComponent implements OnInit {
 
   constructor() { }
+
+  public barChartData: any[] = SAMPLE_DATA;
+  public barChartLabels: string[] = SAMPLE_DATA_LABELS;
+  public barChartType = 'bar';
+  public barChartLegend = 'true';
+  public barChartOptions: any = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
 
   ngOnInit() {
   }
